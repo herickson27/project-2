11ce0b9bcffb7d592b74297842fcfa25933fbb46
Hannah Erickson <hannaherickson@Hannahs-MacBook-Air.local>
2019-03-08 14:50:03 -0800

    heroku

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -4,15 +4,4 @@ const db = require('..models');
 const router = express.Router()
 
 
-app.get('dogs/show', function(req, res){
-    db.dogs.findbyId(req.params.id).then(function(doggo){
-        let apiId = dog.id;
-        let url = ""
-        request(url, function(error, response, body){
-            let image = JSON.parse(body);
-            let desc = JSON.parse(body).description.$t
-            let 
-        })
-    })
-})
 module.exports = router;
